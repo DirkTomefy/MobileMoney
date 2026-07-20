c819f08c8a53faa562ecb446e495520297dcf290
DirkTomefy <DirkTomefy@gmail.com>
2026-07-20 11:33:09 +0300

    [feat] : créer le controller des Tarfis

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,5 +2,10 @@
 
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\DashboardController;
+use App\Controllers\TarifController;
+
 
 $routes->get('backoffice/dashboard', 'DashboardController::index');
+$routes->get('backoffice/tarif', [TarifController::class, 'index']);
+$routes->get('backoffice/tarif/getTarifs', [TarifController::class, 'getTarifs']);
+$routes->post('backoffice/tarif/update', [TarifController::class, 'update']);
